69b81036855b3e16f39375afdb63b6dce1d71ee8
Jim Brandt <jbrandt@bestpractical.com>
2023-04-21 08:50:04 -0400

    Change redirect status code for turbo

--- lib/RT/Interface/Web.pm
+++ lib/RT/Interface/Web.pm
@@ -1129,11 +1129,12 @@ sub Redirect {
 
     # not sure why, but on some systems without this call mason doesn't
     # set status to 302, but 200 instead and people see blank pages
-    $HTML::Mason::Commands::r->status(302);
+    # Turbo wants this to be 303
+    $HTML::Mason::Commands::r->status(303);
 
     # Perlbal expects a status message, but Mason's default redirect status
     # doesn't provide one. See also rt.cpan.org #36689.
-    $HTML::Mason::Commands::m->redirect( $uri->canonical, "302 Found" );
+    $HTML::Mason::Commands::m->redirect( $uri->canonical, "303 See other" );
 
     $HTML::Mason::Commands::m->abort;
 }
